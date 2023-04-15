ea6d942237fde0c0e1881ba211c29351901686f6
pac0sanch3 <noriegatrujolloj123@gmail.com>
2023-04-15 14:32:43 -0500

    subiendo botones

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 alert("Espisificaciones hardware y software")
+function comprobar(){
+    let responsable
+    responsable=document.getElementById("responsable").value
+    document.getElementById("respuesta1").innerHTML="<h3> responsable:"+responsable+"</h3>"
+    console.log(responsable)
+    
+    let producto
+    document.getElementById("producto").value
+    document.getElementById("respuesta2").innerHTML="<h3> nombre del producto:"+producto+"</h3>"
+    console.log(producto)
+}
