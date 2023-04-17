78993046346c4107926349532c249261c2e3b021
jhondepawivanegas <jhondepawivanegas@gmail.com>
2023-04-17 15:10:09 -0500

    subiendo los camiesititos

--- js/script.js
+++ js/script.js
@@ -3,10 +3,120 @@ function comprobar(){
     let responsable
     responsable=document.getElementById("responsable").value
     document.getElementById("respuesta1").innerHTML="<h3> responsable:"+responsable+"</h3>"
-    console.log(responsable)
     
     let producto
-    document.getElementById("producto").value
+    producto=document.getElementById("producto").value
     document.getElementById("respuesta2").innerHTML="<h3> nombre del producto:"+producto+"</h3>"
-    console.log(producto)
+    
+    let linea
+    linea=document.getElementById("linea").value
+    document.getElementById("respuesta3").innerHTML="<h3> linea de produccion:"+linea+"</h3>"
+
+    let primer_modulo
+    primer_modulo=document.getElementById("primer modulo").value
+    document.getElementById("respuesta4").innerHTML="<h3> primer modulo:"+primer_modulo+"</h3>"
+
+    let segundo_modulo
+    segundo_modulo=document.getElementById("segundo modulo").value
+    document.getElementById("respuesta5").innerHTML="<h3> segundo modulo:"+segundo_modulo+"</h3>"
+
+    let tercer_modulo
+    tercer_modulo=document.getElementById("tercer modulo").value
+    document.getElementById("respuesta6").innerHTML="<h3> nombre del producto:"+tercer_modulo+"</h3>"
+
+    let modulo1
+    modulo1=document.getElementById("modulo1").value
+    document.getElementById("respuesta7").innerHTML="<h3> descripcion del modulo 1:"+modulo1+"</h3>"
+
+    let modulo2 
+    modulo2=document.getElementById("modulo2").value
+    document.getElementById("respuesta8").innerHTML="<h3> descripcion del modulo 2:"+modulo2+"</h3>"
+
+    let modulo3
+    modulo3=document.getElementById("modulo3").value
+    document.getElementById("respuesta9").innerHTML="<h3> descipcion del modulo 3:"+modulo3+"</h3>"
+
+    let DESCRIPCIÓN
+    DESCRIPCIÓN=document.getElementById("DESCRIPCIÓN").value
+    document.getElementById("respuesta10").innerHTML="<h3> descripcion del producto:"+DESCRIPCIÓN+"</h3>"
+
+    let arquitectura
+    arquitectura=document.getElementById("arquitectura").value
+    document.getElementById("respuesta11").innerHTML="<h3> arquitectura del producto:"+arquitectura+"</h3>"
+
+    let requerimientosF
+    requerimientosF=document.getElementById("requerimientosF").value
+    document.getElementById("respuesta12").innerHTML="<h3> requerimientos funcionales:"+requerimientosF+"</h3>"
+
+    let requerimientosA
+    requerimientosA=document.getElementById("requerimientosA").value
+    document.getElementById("respuesta13").innerHTML="<h3> requerimeintos adicionales:"+requerimientosA+"</h3>"
+    
+    let cliente
+    cliente=document.getElementById("cliente").value
+    document.getElementById("respuesta14").innerHTML="<h3> clientes:"+cliente+"</h3>"
+
+    let hardware
+    hardware=document.getElementById("hardware").value
+    document.getElementById("respuesta15").innerHTML="<h3> requerimientos de hardware:"+hardware+"</h3>"
+
+    let software
+    software=document.getElementById("software").value
+    document.getElementById("respuesta16").innerHTML="<h3> requerimientos de software:"+software+"</h3>"
+
+    let otros
+    otros=document.getElementById("otros").value
+    document.getElementById("respuesta17").innerHTML="<h3> otros:"+otros+"</h3>"
+
+    let hardware1
+    hardware1=document.getElementById("hardware1").value
+    document.getElementById("respuesta18").innerHTML="<h3> requerimientos del cliente hardware:"+hardware1+"</h3>"
+
+    let software1
+    software1=document.getElementById("software1").value
+    document.getElementById("respuesta19").innerHTML="<h3> requerimientos del cliente software:"+software1+"</h3>"
+
+    let otros1
+    otros1=document.getElementById("otros1").value
+    document.getElementById("respuesta21").innerHTML="<h3> nombre del producto:"+otros1+"</h3>"
+
+    let primera_version
+    primera_version=document.getElementById("primera version").value
+    document.getElementById("respuesta22").innerHTML="<h3> nombre del producto:"+primera_version+"</h3>"
+
+    let primer_cambio
+    primer_cambio=document.getElementById("primer cambio").value
+    document.getElementById("respuesta23").innerHTML="<h3> nombre del producto:"+primer_cambio+"</h3>"
+
+    let primera_aprobacion
+    primera_aprobacion=document.getElementById("primera aprobacion").value
+    document.getElementById("respuesta24").innerHTML="<h3> nombre del producto:"+primera_aprobacion+"</h3>"
+
+    let primera_validacion
+    primera_validacion=document.getElementById("primera validacion").value
+    document.getElementById("respuesta24").innerHTML="<h3> nombre del producto:"+primera_validacion+"</h3>"
+
+    let segunda_version
+    segunda_version=document.getElementById("segunda version").value
+    document.getElementById("respuesta25").innerHTML="<h3> nombre del producto:"+segunda_version+"</h3>"
+
+    let segundo_cambio
+    segundo_cambio=document.getElementById("segundo cambio").value
+    document.getElementById("respuesta26").innerHTML="<h3> nombre del producto:"+segundo_cambio+"</h3>"
+
+    let segunda_aprobacion
+    segunda_aprobacion=document.getElementById("segunda aprobacion").value
+    document.getElementById("respuesta27").innerHTML="<h3> nombre del producto:"+segunda_aprobacion+"</h3>"
+
+    let segunda_validacion
+    segunda_validacion=document.getElementById("segunda_validacion").value
+    document.getElementById("respuesta28").innerHTML="<h3> nombre del producto:"+segunda_validacion+"</h3>"
+
+    let firma1
+    firma1=document.getElementById("firma1").value
+    document.getElementById("respuesta29").innerHTML="<h3> primera firma:"+firma1+"</h3>"
+
+    let firma2
+    firma2=document.getElementById("firma2").value
+    document.getElementById("respuesta30").innerHTML="<h3> segunda firma:"+firma2+"</h3>"
 }
